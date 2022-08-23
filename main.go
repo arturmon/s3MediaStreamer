c1f94591e9441e08cce7e6588755447c7a77f41e
Мудрых Артур Викторович (4047650) <amudryh@vtb.ru>
2022-08-24 02:03:03 +0300

    fix version swagger BasePath add test api

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 	docs.SwaggerInfo.Description = "This is a sample server albums server."
 	docs.SwaggerInfo.Version = "1.0"
 	docs.SwaggerInfo.Host = "localhost:8080"
-	docs.SwaggerInfo.BasePath = "/v2"
+	docs.SwaggerInfo.BasePath = "/v1"
 	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 
 	// Gin instance
