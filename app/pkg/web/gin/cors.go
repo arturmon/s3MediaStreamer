9ac684d9ede058d337d23abe0b9bcc4ea04a1d90
Artur Mudrykh <a.mudrykh@uzum.com>
2023-07-27 13:04:15 +0300

    fix: Fix code scanning alert - Clear-text logging of sensitive information

--- app/pkg/web/gin/cors.go
+++ app/pkg/web/gin/cors.go
@@ -1,14 +1,13 @@
 package gin
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
-		fmt.Println(c.Request.Header)
+		//fmt.Println(c.Request.Header)
 		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
 		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
