6da5823f8a75c0d00145ad37586f096276f31122
Artur Mudrykh <a.mudrykh@uzum.com>
2023-08-16 02:25:45 +0300

    feat: update exclude-rules

--- app/pkg/monitoring/healthcheck_test.go
+++ app/pkg/monitoring/healthcheck_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// Mocking the config.AppHealth variable to return true
+// Mocking the config.AppHealth variable to return true.
 var mockAppHealth = true
 
 func mockHealthGET(c *gin.Context) {
@@ -19,23 +19,23 @@ func mockHealthGET(c *gin.Context) {
 }
 
 func TestHealthGET(t *testing.T) {
-	// Initialize a new Gin engine with the mockHandler
+	// Initialize a new Gin engine with the mockHandler.
 	r := gin.New()
 	r.GET("/health", mockHealthGET)
 
-	// Create a request to the "/health" endpoint
+	// Create a request to the "/health" endpoint.
 	req, _ := http.NewRequest("GET", "/health", http.NoBody) // Use http.NoBody instead of nil
 
-	// Create a response recorder to record the response
+	// Create a response recorder to record the response.
 	w := httptest.NewRecorder()
 
-	// Serve the request using the router
+	// Serve the request using the router.
 	r.ServeHTTP(w, req)
 
-	// Check the response status code to ensure the request was successful (HTTP 200 OK)
+	// Check the response status code to ensure the request was successful (HTTP 200 OK).
 	assert.Equal(t, http.StatusOK, w.Code)
 
-	// You can also check the response body or headers as needed
-	// For example, you can assert that the response contains {"status": "UP"} in the JSON body:
+	// You can also check the response body or headers as needed.
+	// For example, you can assert that the response contains {"status": "UP"} in the JSON body:.
 	assert.JSONEq(t, `{"status": "UP"}`, w.Body.String())
 }
