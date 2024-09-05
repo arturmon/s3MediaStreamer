678d71effee204b9175c384e6d8db0572b2e74e2
Artur Mudrykh <arturmon82@gmail.com>
2024-09-05 16:54:20 +0300

    feat: add caching tracks service

--- app/services/cashing/cashing_service.go
+++ app/services/cashing/cashing_service.go
@@ -1,10 +1,17 @@
 package cashing
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 type CachingRepository interface {
 	CachePasswordVerificationInRedis(ctx context.Context, userID string, verificationSuccess bool, expiration int) error
 	CheckPasswordVerificationInRedis(ctx context.Context, userID string) (bool, bool, error)
+	GetTrackInCache(ctx context.Context, key string, result interface{}) error
+	SetTrackInCache(ctx context.Context, key string, value interface{}, ttl time.Duration) error
+	UpdateTrackInCache(ctx context.Context, key string, value interface{}, ttl time.Duration) error
+	DeleteTrackInCache(ctx context.Context, key string) error
 }
 
 type CachingService struct {
@@ -21,4 +28,20 @@ func (s *CachingService) CachePasswordVerificationInRedis(ctx context.Context, u
 
 func (s *CachingService) CheckPasswordVerificationInRedis(ctx context.Context, userID string) (bool, bool, error) {
 	return s.redisRepository.CheckPasswordVerificationInRedis(ctx, userID)
+}
+
+func (s *CachingService) GetTrackInCache(ctx context.Context, key string, result interface{}) error {
+	return s.redisRepository.GetTrackInCache(ctx, key, result)
+}
+
+func (s *CachingService) SetTrackInCache(ctx context.Context, key string, value interface{}, ttl time.Duration) error {
+	return s.redisRepository.SetTrackInCache(ctx, key, value, ttl)
+}
+
+func (s *CachingService) UpdateTrackInCache(ctx context.Context, key string, value interface{}, ttl time.Duration) error {
+	return s.redisRepository.UpdateTrackInCache(ctx, key, value, ttl)
+}
+
+func (s *CachingService) DeleteTrackInCache(ctx context.Context, key string) error {
+	return s.redisRepository.DeleteTrackInCache(ctx, key)
 }
