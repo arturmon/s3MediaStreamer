15dd7534c19ec9359796efe1c23f3320f24abe69
Artur Mudrykh <arturmon82@gmail.com>
2024-09-05 23:13:10 +0300

    feat: delete manual track caching func

--- app/repository/cashing/cashing_repository.go
+++ app/repository/cashing/cashing_repository.go
@@ -2,9 +2,7 @@ package cashing
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
-	"fmt"
 	"s3MediaStreamer/app/internal/logs"
 	"strconv"
 	"time"
@@ -15,18 +13,11 @@ import (
 type CachingInterface interface {
 	CachePasswordVerificationInRedis(ctx context.Context, userID string, verificationSuccess bool, expiration int) error
 	CheckPasswordVerificationInRedis(ctx context.Context, userID string) (bool, bool, error)
-	GetTrackInCache(ctx context.Context, key string, result interface{}) error
-	SetTrackInCache(ctx context.Context, key string, value interface{}, ttl time.Duration) error
-	UpdateTrackInCache(ctx context.Context, key string, value interface{}, ttl time.Duration) error
-	DeleteTrackInCache(ctx context.Context, key string) error
 }
 
 type CachingRepository struct {
 	redisClient *redis.Client
 }
-
-// Assuming you have a Redis client configured.
-// var redisClient *redis.Client
 
 // InitRedisRepository initializes the Redis client.
 func InitRedisRepository(logger *logs.Logger, redisClient *redis.Client) *CachingRepository {
@@ -64,42 +55,4 @@ func (c *CachingRepository) CheckPasswordVerificationInRedis(ctx context.Context
 
 	// The result was found and successfully converted.
 	return true, verificationSuccess, nil
-}
-
-func (c *CachingRepository) GetTrackInCache(ctx context.Context, key string, result interface{}) error {
-	data, err := c.redisClient.Get(ctx, key).Result()
-	if errors.Is(err, redis.Nil) {
-		// Data not found in cache
-		return fmt.Errorf("cache miss")
-	} else if err != nil {
-		return err
-	}
-
-	// Unmarshal JSON data into the result object
-	return json.Unmarshal([]byte(data), result)
-}
-
-func (c *CachingRepository) SetTrackInCache(ctx context.Context, key string, value interface{}, ttl time.Duration) error {
-	data, err := json.Marshal(value)
-	if err != nil {
-		return err
-	}
-
-	return c.redisClient.Set(ctx, key, data, ttl).Err()
-}
-
-// UpdateTrackInCache updates existing track data in Redis if the track data has changed.
-func (c *CachingRepository) UpdateTrackInCache(ctx context.Context, key string, value interface{}, ttl time.Duration) error {
-	// Delete existing cache first
-	err := c.DeleteTrackInCache(ctx, key)
-	if err != nil {
-		return fmt.Errorf("failed to delete existing cache: %w", err)
-	}
-
-	// Set the updated value in cache
-	return c.SetTrackInCache(ctx, key, value, ttl)
-}
-
-func (c *CachingRepository) DeleteTrackInCache(ctx context.Context, key string) error {
-	return c.redisClient.Del(ctx, key).Err()
 }
