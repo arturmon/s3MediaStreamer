7a7c4333b04eec7da88b66a064c07851fbcb472e
Artur Mudrykh <arturmon82@gmail.com>
2024-10-22 11:48:29 +0300

    chore: clean

--- app/internal/logs/logs_types.go
+++ app/internal/logs/logs_types.go
@@ -6,32 +6,34 @@ import (
 	"os"
 )
 
+// Grouped logging helper that adds a group with a name and logs the message.
+
 func (l *Logger) Infof(format string, args ...any) {
 	// Format the message using fmt.Sprintf
 	msg := fmt.Sprintf(format, args...)
 
-	l.Info(msg) // Log the formatted message (make sure you have an Info method)
+	l.Info(msg)
 }
 
 func (l *Logger) Debugf(format string, args ...any) {
 	// Format the message using fmt.Sprintf
 	msg := fmt.Sprintf(format, args...)
 
-	l.Debug(msg) // Log the formatted message (make sure you have a Debug method)
+	l.Debug(msg)
 }
 
 func (l *Logger) Warnf(format string, args ...any) {
 	// Format the message using fmt.Sprintf
 	msg := fmt.Sprintf(format, args...)
 
-	l.Warn(msg) // Log the formatted message (make sure you have a Warn method)
+	l.Warn(msg)
 }
 
 func (l *Logger) Errorf(format string, args ...any) {
 	// Format the message using fmt.Sprintf
 	msg := fmt.Sprintf(format, args...)
 
-	l.Error(msg) // Log the formatted message
+	l.Error(msg)
 }
 
 // Fatal logs a message at the error level and then exits.
@@ -42,14 +44,14 @@ func (l *Logger) Fatal(msg string, attrs ...slog.Attr) {
 		args[i] = attr
 	}
 
-	l.Error(msg, args...) // Log error
-	os.Exit(1)            // Exit the program
+	l.Error(msg, args...)
+	os.Exit(1)
 }
 
 func (l *Logger) Fatalf(format string, args ...any) {
 	// Format the message using fmt.Sprintf
 	msg := fmt.Sprintf(format, args...)
 
-	l.Error(msg) // Log error
-	os.Exit(1)   // Exit the program
+	l.Error(msg)
+	os.Exit(1)
 }
