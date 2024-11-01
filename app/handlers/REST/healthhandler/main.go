7e85bce4bdc4c8a2b63eb9a6e2f792b51bfa3d98
Artur Mudrykh <arturmon82@gmail.com>
2024-11-01 21:07:35 +0200

    feat: delete swagger info health endpoint

--- app/handlers/REST/healthhandler/main.go
+++ app/handlers/REST/healthhandler/main.go
@@ -19,16 +19,6 @@ func NewMonitoringHandler(monitoringService health.Service) *Handler {
 	return &Handler{monitoringService}
 }
 
-// LivenessGET godoc
-// @Summary Get liveness status of the application
-// @Description Checks and returns the liveness status of the application
-// @Tags health-controller
-// @Accept  */*
-// @Produce json
-// @BasePath	/
-// @Success 200 {object} model.LivenessResponse
-// @Failure 502 {object} model.ErrorResponse "Internal Server Error"
-// @Router /health/liveness [get]
 func (h *Handler) LivenessGET(c *gin.Context, wrapper *health.Service) {
 	// Use pingDatabase for liveness probe
 	err := wrapper.DBRepository.Ping(c)
@@ -44,16 +34,6 @@ func (h *Handler) LivenessGET(c *gin.Context, wrapper *health.Service) {
 	})
 }
 
-// ReadinessGET godoc
-// @Summary Get readiness status of the application
-// @Description Checks and returns the readiness status of the application
-// @Tags health-controller
-// @Accept  */*
-// @Produce json
-// @BasePath	/
-// @Success 200 {object} model.ReadinessResponse
-// @Failure 502 {object} model.ErrorResponse "Internal Server Error"
-// @Router /health/readiness [get]
 func (h *Handler) ReadinessGET(c *gin.Context, wrapper *health.Service) {
 	// wrapper.HealthMetrics.Mutex.Lock()
 	// defer wrapper.HealthMetrics.Mutex.Unlock()
